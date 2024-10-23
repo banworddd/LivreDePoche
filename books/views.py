e566d7ddb408f5b0047e58f312d18f8ed9496f86
banworddd <imsva1089@icloud.com>
2024-10-23 12:40:52 +0300

    Added reviews on books

--- books/views.py
+++ books/views.py
@@ -1,13 +1,66 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book, Author
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
+from books.models import Book, Author
+from users.models import ReadingList, CurrentlyReadingList, PlannedReadingList, BookReview
 def books_view(request):
     books = Book.objects.all()
     return render(request, 'books/books.html', {'books': books})
 
 def book_detail_view(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    return render(request, 'books/book_detail.html', {'book': book})
+
+    # Проверка наличия отзывов на книгу
+    reviews = BookReview.objects.filter(book=book)
+
+    # Проверка, если у пользователя есть книга в списках
+    is_read = ReadingList.objects.filter(user=request.user, book=book).exists()
+    is_reading = CurrentlyReadingList.objects.filter(user=request.user, book=book).exists()
+    is_planned = PlannedReadingList.objects.filter(user=request.user, book=book).exists()
+
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        if action == 'read':
+            ReadingList.objects.get_or_create(user=request.user, book=book)
+        elif action == 'reading':
+            CurrentlyReadingList.objects.get_or_create(user=request.user, book=book)
+        elif action == 'planned':
+            PlannedReadingList.objects.get_or_create(user=request.user, book=book)
+
+        # После добавления перенаправляем на ту же страницу
+        return redirect('book_detail', book_id=book.id)
+
+    return render(request, 'books/book_detail.html', {
+        'book': book,
+        'reviews': reviews,
+        'is_read': is_read,
+        'is_reading': is_reading,
+        'is_planned': is_planned
+    })
+
+
+
+@login_required
+def add_review(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == 'POST':
+        rating = request.POST.get('rating')
+        review_text = request.POST.get('review_text')
+
+        # Создаем новый отзыв
+        BookReview.objects.create(
+            user=request.user,
+            book=book,
+            rating=rating,
+            review_text=review_text,
+            review_date=timezone.now()
+        )
+        return redirect('book_detail', book_id=book.id)
+
+    return redirect('book_detail', book_id=book.id)
+
 def authors_view(request):
     authors = Author.objects.all()
     return render(request, 'books/authors.html', {'authors': authors})
--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import AbstractUser
+
 from books.models import Book
 
 class CustomUser(AbstractUser):
@@ -29,3 +31,13 @@ class PlannedReadingList(models.Model):
 
     def __str__(self):
         return f"{self.user.username} plans to read {self.book.title}"
+
+class BookReview(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    review_text = models.TextField()
+    review_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} says {self.review_text} on {self.review_date}'
