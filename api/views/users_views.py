57b9fc2b21246c624d87c545cc6aa3ef3707cd57
banworddd <imsva1089@gmail.com>
2024-12-10 14:44:35 +0300

    fix friends APIs

--- api/views/users_views.py
+++ api/views/users_views.py
@@ -178,24 +178,22 @@ class SendFriendRequestView(CreateAPIView):
     serializer_class = FriendsListSerializer
 
     def create(self, request, *args, **kwargs):
-        user = request.user
-        friend_id = request.data.get('friend_id')
-
-        if not friend_id:
-            return Response({"detail": "Friend ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+        friend_username = request.data.get('friend_username')
+        if not friend_username:
+            return Response({"detail": "Friend username is required."}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            friend = CustomUser.objects.get(id=friend_id)
+            friend = CustomUser.objects.get(username=friend_username)
         except CustomUser.DoesNotExist:
             return Response({"detail": "Friend not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        if FriendsList.objects.filter(user=user, friend=friend, status='waiting').exists():
+        if FriendsList.objects.filter(user=request.user, friend=friend, status='waiting').exists():
             return Response({"detail": "Friend request already sent."}, status=status.HTTP_400_BAD_REQUEST)
 
-        if FriendsList.objects.filter(user=user, friend=friend, status='accepted').exists():
+        if FriendsList.objects.filter(user=request.user, friend=friend, status='accepted').exists():
             return Response({"detail": "You are already friends."}, status=status.HTTP_400_BAD_REQUEST)
 
-        friend_request = FriendsList.objects.create(user=user, friend=friend, status='waiting')
+        friend_request = FriendsList.objects.create(user=request.user, friend=friend, status='waiting')
         serializer = FriendsListSerializer(friend_request)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -222,13 +220,12 @@ class AcceptFriendRequestView(UpdateAPIView):
 class DeleteFriendRequestView(DestroyAPIView):
     queryset = FriendsList.objects.all()
     serializer_class = FriendsListSerializer
-    lookup_field = 'id'
+    lookup_field = 'pk'
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 
 
